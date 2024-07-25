baa342ed126661ac9b2883c163ab953dbb04ca38
amineelhassak <amineatrache57@gmail.com>
2024-07-25 23:38:28 +0100

    new update

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,11 +1,17 @@
 #include "DiamondTrap.hpp"
 
-int main()
+int main( void )
 {
+        DiamondTrap diamondtrap;
+
         DiamondTrap diamondtrap2("Shiny");
+
         std::string str = "Target";
+
         diamondtrap2.attack(str);
+
         diamondtrap2.takeDamage(30);
+        
         diamondtrap2.beRepaired(40);
 
         diamondtrap2.guardGate();
@@ -13,9 +19,4 @@ int main()
         diamondtrap2.highFivesGuys();
 
         diamondtrap2.whoAmI();
-
-        // ClapTrap *polymorphic_trap = &diamondtrap2;
-        // polymorphic_trap->attack("polymorphic target");
-
-        // diamondtrap2.ClapTrap::attack("direct ClapTrap call");
 }
